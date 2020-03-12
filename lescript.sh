c3c5aca1704409deec4d0494b4355e14ecc4f6d4
Mathieu <mathdarribau33@gmail.com>
2020-03-12 10:59:12 +0100

    version2

--- lescript.sh
+++ lescript.sh
@@ -2,12 +2,30 @@
 
 echo "hello word"
 
+  if [ -e /tmp/out/lock ]
+     then
+
+     exit 22
+
+  else
+touch /tmp/out/lock
+
 for fich in /tmp/in/*
 do
   if [ -f $fich ]
     then
-    gzip $fich
+    
+    mv $fich /tmp/out/ 
 
-    mv $fich /tmp/out/
+  
+
+    gzip /tmp/out/ $fich  
   fi
+
+
 done
+
+rm /tmp/out/lock
+
+fi
+
